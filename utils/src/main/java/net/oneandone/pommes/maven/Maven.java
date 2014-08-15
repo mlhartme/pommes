9def327498bd6ba71b9699e4e9361e53f1b91443
Michael Hartmeier <mlhartme@github.com>
2014-08-15 20:50:51 +0200

    align package name

--- utils/src/main/java/net/oneandone/pommes/maven/Maven.java
+++ utils/src/main/java/net/oneandone/pommes/maven/Maven.java
@@ -13,7 +13,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package net.oneandone.sales.tools.maven;
+package net.oneandone.pommes.maven;
 
 import net.oneandone.sushi.fs.World;
 import net.oneandone.sushi.fs.file.FileNode;
