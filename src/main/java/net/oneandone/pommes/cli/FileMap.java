27ae4b407cf197498a4aa854bae54edefdf0d7da
Michael Hartmeier <mlhartme@github.com>
2014-08-19 17:11:26 +0200

    package naming cleanup

--- src/main/java/net/oneandone/pommes/cli/FileMap.java
+++ src/main/java/net/oneandone/pommes/cli/FileMap.java
@@ -13,7 +13,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package net.oneandone.pommes;
+package net.oneandone.pommes.cli;
 
 import net.oneandone.sushi.fs.LineFormat;
 import net.oneandone.sushi.fs.LineReader;
--- src/main/java/net/oneandone/pommes/cli/Main.java
+++ src/main/java/net/oneandone/pommes/cli/Main.java
@@ -13,7 +13,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package net.oneandone.pommes;
+package net.oneandone.pommes.cli;
 
 import net.oneandone.maven.embedded.Maven;
 import net.oneandone.sushi.cli.Child;
