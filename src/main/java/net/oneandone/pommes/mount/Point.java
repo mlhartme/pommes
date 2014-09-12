6fc795d01562bb6c5575f1d548b4c09e22c72b5b
Michael Hartmeier <mlhartme@github.com>
2014-09-12 22:05:42 +0200

    docs

--- src/main/java/net/oneandone/pommes/mount/Point.java
+++ src/main/java/net/oneandone/pommes/mount/Point.java
@@ -57,7 +57,10 @@ public class Point {
     }
 
 
-    /** @return url with tailing slash or null */
+    /**
+     * CAUTION: it's not possible to not "unfold" directory names into the original url.
+     * @return url with tailing slash or null
+     */
     public String svnurl(FileNode childDirectory) {
         return childDirectory.hasAnchestor(directory) ? uri + childDirectory.getRelative(directory) + "/" : null;
     }
