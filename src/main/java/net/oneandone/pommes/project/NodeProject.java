36b259003d2af68de712e603449f5c04074d87a3
Michael Hartmeier <mlhartme@github.com>
2016-12-16 08:19:28 +0100

    scm fix

--- src/main/java/net/oneandone/pommes/project/NodeProject.java
+++ src/main/java/net/oneandone/pommes/project/NodeProject.java
@@ -15,47 +15,13 @@
  */
 package net.oneandone.pommes.project;
 
-import net.oneandone.pommes.cli.Environment;
-import net.oneandone.pommes.model.Pom;
-import net.oneandone.pommes.repository.NodeRepository;
 import net.oneandone.sushi.fs.Node;
-
-import java.io.IOException;
 
 /** Factory for Poms */
 public abstract class NodeProject extends Project {
-    private String overrideOrigin = null;
-    private String overrideRevision = null;
-
-    //--
-
     protected final Node file;
 
     protected NodeProject(Node file) {
         this.file = file;
-    }
-
-    @Override
-    public Pom load(Environment environment, String zone) throws IOException {
-        return doLoad(environment, zone, getOrigin(), overrideRevision == null ? Long.toString(file.getLastModified()) : overrideRevision,
-                NodeRepository.nodeScm(file));
-    }
-
-    protected abstract Pom doLoad(Environment environment, String zone, String origin, String revision, String scm) throws IOException;
-
-    public void setOrigin(String origin) {
-        this.overrideOrigin = origin;
-    }
-
-    public void setRevision(String revision) {
-        this.overrideRevision = revision;
-    }
-
-    private String getOrigin() {
-        return overrideOrigin == null ? file.getUri().toString() : overrideOrigin;
-    }
-
-    public String toString() {
-        return getOrigin();
     }
 }
